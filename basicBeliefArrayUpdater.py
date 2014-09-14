a84d55c51fc2949220266515d98a30732ac5c9e6
samjg60 <samjg60@gmail.com>
2014-09-14 19:20:59 +0100

    Make program more general
    Allow multiple measurements in the updating process

--- basicBeliefArrayUpdater.py
+++ basicBeliefArrayUpdater.py
@@ -2,8 +2,8 @@
 p = [0.2, 0.2, 0.2, 0.2, 0.2]
 # Initialize world
 world = ['green', 'red', 'red', 'green', 'green']
-# Sensor result
-Z = 'red'
+# Sensor result array
+measurements = ['red', 'green']
 # Sensor reliabilities
 pHit = 0.6
 pMiss = 0.2
@@ -13,14 +13,23 @@ def sense(p, Z):
 	q = []
 
 	for i in range(len(p)):
+
+		# Set hit to 1 if Z equals world[i] tile colour, else Z = 0
 		hit = (Z == world[i])
+
+		# Append a belief to q for each tile. If hit == 1, then
+		# q[i]=p[i]*pHit, else q[i]=p[i]*pMiss
 		q.append(p[i] * (hit * pHit + (1 - hit) * pMiss))
 
 	a = sum(q)
 
+	# Normalize the values of q so they sum(q)==1
 	for i in range(len(p)):
 		q[i] = q[i]/a
 
 	return q
 
-print sense(p, Z)
+for k in range(len(measurements)):
+	p = sense(p, k)
+
+print p
